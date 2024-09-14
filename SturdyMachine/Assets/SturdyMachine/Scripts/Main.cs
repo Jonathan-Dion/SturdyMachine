e3f6c976b3dd5a0965a30f84ef50459b7b5cd334
Noydj <50801604+Jonathan-Dion@users.noreply.github.com>
2024-09-14 17:31:19 -0400

    Add autorisation when attack any direction when Skinny is on Stagger StateMode

--- SturdyMachine/Assets/SturdyMachine/Scripts/Main.cs
+++ SturdyMachine/Assets/SturdyMachine/Scripts/Main.cs
@@ -205,7 +205,8 @@ namespace SturdyMachine.Manager
             for (int i = 0; i < _ennemyBot.Length; ++i)
                 _ennemyBot[i].OnUpdate(_featureManager.GetFightsModule.GetCurrentFightOffenseData().offense.GetOffenseDirection, _featureManager.GetFightsModule.GetCurrentFightOffenseData().offense.GetOffenseType, false, false, _featureManager.GetStateConfirmModule.GetEnemyAnimationClipOffenseType, _featureManager.GetStateConfirmModule.GetEnemyAnimationClipOffenseType == AnimationClipOffenseType.Stagger);
 
-            _featureManager.OnUpdate(_sturdyInputControl.GetIsLeftFocusActivated, _sturdyInputControl.GetIsRightFocusActivated, _featureManager.GetHitConfirmModule.GetIsGoodOffenseDirection());
+            _featureManager.OnUpdate(_sturdyInputControl.GetIsLeftFocusActivated, _sturdyInputControl.GetIsRightFocusActivated, 
+                _featureManager.GetHitConfirmModule.GetIsGoodOffenseDirection(_featureManager.GetEnemyBotFocusedOffenseManager.GetCurrentOffense.GetOffenseDirection, _featureManager.GetPlayerBotOffenseManager.GetCurrentOffense.GetOffenseDirection, _featureManager.GetPlayerBotOffenseManager.GetCurrentOffense));
         }
 
         void LateUpdate()
